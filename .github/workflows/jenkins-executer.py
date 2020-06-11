208df3856006b17e694fb98bbb0ea1f1ecad8878
Víctor Martínez Bevià <6623606+vicmarbev@users.noreply.github.com>
2020-06-11 16:29:56 +0200

    Update jenkins-executer.py

--- .github/workflows/jenkins-executer.py
+++ .github/workflows/jenkins-executer.py
@@ -25,7 +25,6 @@ r = requests.post(start_build_url)
 
 # from return headers get job queue location
 #
-print(r.headers['Location'])
 m = re.match(r"http.+(queue.+)\/", r.headers['Location'])
 if not m:
     # To Do: handle error
