b24d590414de6695c3122e802ed4e8cf3c5c01e1
Víctor Martínez Bevià <6623606+vicmarbev@users.noreply.github.com>
2020-06-11 16:08:12 +0200

    Update jenkins-executer.py

--- .github/workflows/jenkins-executer.py
+++ .github/workflows/jenkins-executer.py
@@ -36,7 +36,7 @@ if not m:
 queue_id = m.group(1)
 job_info_url = 'http://{}@{}/{}/api/json'.format(auth_token, jenkins_uri, queue_id)
 elasped_time = 0 
-print '{} Job {} added to queue: {}'.format(time.ctime(), job_name, job_info_url)
+print('{} Job {} added to queue: {}'.format(time.ctime(), job_name, job_info_url))
 while True:
     l = requests.get(job_info_url)
     jqe = l.json()
@@ -50,36 +50,36 @@ while True:
         elasped_time += QUEUE_POLL_INTERVAL
 
     if (elasped_time % (QUEUE_POLL_INTERVAL * 10)) == 0:
-        print "{}: Job {} not started yet from {}".format(time.ctime(), job_name, queue_id)
+        print("{}: Job {} not started yet from {}".format(time.ctime(), job_name, queue_id))
 
 # poll job status waiting for a result
 #
 job_url = 'http://{}@{}/job/{}/{}/api/json'.format(auth_token, jenkins_uri, job_name, job_id)
 start_epoch = int(time.time())
 while True:
-    print "{}: Job started URL: {}".format(time.ctime(), job_url)
+    print("{}: Job started URL: {}".format(time.ctime(), job_url))
     j = requests.get(job_url)
     jje = j.json()
     result = jje['result']
     if result == 'SUCCESS':
         # Do success steps
-        print "{}: Job: {} Status: {}".format(time.ctime(), job_name, result)
+        print("{}: Job: {} Status: {}".format(time.ctime(), job_name, result))
         break
     elif result == 'FAILURE':
         # Do failure steps
-        print "{}: Job: {} Status: {}".format(time.ctime(), job_name, result)
+        print("{}: Job: {} Status: {}".format(time.ctime(), job_name, result))
         sys.exit(1)
     elif result == 'ABORTED':
         # Do aborted steps
-        print "{}: Job: {} Status: {}".format(time.ctime(), job_name, result)
+        print("{}: Job: {} Status: {}".format(time.ctime(), job_name, result))
         break
     else:
-        print "{}: Job: {} Status: {}. Polling again in {} secs".format(
-                time.ctime(), job_name, result, JOB_POLL_INTERVAL)
+        print("{}: Job: {} Status: {}. Polling again in {} secs".format(
+                time.ctime(), job_name, result, JOB_POLL_INTERVAL))
 
     cur_epoch = int(time.time())
     if (cur_epoch - start_epoch) > OVERALL_TIMEOUT:
-        print "{}: No status before timeout of {} secs".format(OVERALL_TIMEOUT)
+        print("{}: No status before timeout of {} secs".format(OVERALL_TIMEOUT))
         sys.exit(1)
 
     time.sleep(JOB_POLL_INTERVAL)
